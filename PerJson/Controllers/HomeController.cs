2a185f96460fc197b5eb98b96618604ee964b715
olafn <olafn@NIT-PC2>
2016-09-28 14:37:16 +0200

    Add project files.

---
+++ PerJson/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PerJson.PerService;
+using System.Web.Helpers;
+ 
+namespace PerJson.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+         
+            JsonResult gg = new JsonResult();
+            System.String tt;
+            Person pp;
+            PersonServiceClient ps = new PersonServiceClient();
+            LookupParameters lookup = new LookupParameters();
+            lookup.NIN = "01090097365";
+            ps.ClientCredentials.UserName.UserName = "test";
+            ps.ClientCredentials.UserName.Password = "BF32511";
+            pp = ps.GetPerson(lookup);
+            
+          gg.Data = Json();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            ViewBag.Message = "Din beskr her.";
+            
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PerJson/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PerJson.Startup))]
+namespace PerJson
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
